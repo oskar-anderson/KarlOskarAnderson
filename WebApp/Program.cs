28f463e494ef00d9d637be34fd1bcdb05eb09276
oskar-anderson <andersonkarloskar@gmail.com>
2023-07-27 09:37:34 +0300

    Add app.UsePathBase("/KarlOskarAnderson");

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -30,6 +30,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UsePathBase("/KarlOskarAnderson");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
